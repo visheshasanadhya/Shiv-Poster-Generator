5bb842cd447990616dfd30b032530c356cfe19c9
visheshasanadhya <sanadhya2003@gmail.com>
2025-07-14 23:27:28 +0530

    Deploy Flutter Web to docs folder

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"1425e5e9ec5eeb4f225c401d8db69b860e0fde
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "228422594"
+    serviceWorkerVersion: "3139986022"
   }
 });
